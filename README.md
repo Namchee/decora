4802cb0cb8aa0cada6d9112545d8f33ec8d08d91
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-02-03 13:48:28 +0700

    feat: Add benchmark decorator

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# decorator-ku
-Useful TypeScript's decorator for personal purposes
+# Decorator Ku
+
+Useful ECMAScript decorator for personal purposes.
+
+Although it's written in [TypeScript](https://www.typescriptlang.org/), these decorators should be compatible with normal JS files as long as it supports ES7.
+
+Will be updated indefinitely.
+
+## `@benchmark`
+
+**Decorator Type**: Method decorator
+
+Add benchmarking functionality to a method from a class. Benchmark result will be written to `console`.
+
+```ts
+class Example {
+  @benchmark()
+  logMe() {
+    console.log('test');
+  }
+}
+```
+
+### Parameters
+
+**Name** | **Required?** | **Values** | **Default Value** | **Description**
+---- | --------- | ------ | ------------- | -----------
+`metric` | `false` | `'s' | 'ms' | 'ns'` | `'ms'` | Metric to be used when logging function runtime
+`precision` | `false` | `number, n > 1` | `undefined` | Number of digits after comma. Passing `undefined` will print an abruptly long string.
---
+++ index.ts
@@ -0,0 +1,50 @@
+import { performance } from 'perf_hooks';
+
+/**
+ * Apply simple benchmarking to a method,
+ * which will logged to `console`.
+ * @param {'s' | 'ms' | 'ms'} metric - time metric to be used.
+ * Defaults to `ms`
+ * @param {number?} precision - time precision to be shown on
+ * log. Passing `undefined` will log an abruptly long number string.
+ * @return {Function} - modified function with benchmark capabilities
+ */
+export function benchmark(
+  metric: 's' | 'ms' | 'ns' = 'ms',
+  precision?: number,
+): Function {
+  return function(
+    target: Object,
+    keyName: string,
+    descriptor: PropertyDescriptor,
+  ): void {
+    if (!(descriptor.value instanceof Function)) {
+      throw new Error(
+        '`benchmark` decorator can only be applied to functions',
+      );
+    }
+
+    const fn: Function = descriptor.value;
+
+    descriptor.value = async function(...args: any[]) {
+      const start = performance.now();
+
+      const result = await fn.apply(this, args);
+
+      const end = performance.now();
+      let diff = end - start;
+
+      if (metric !== 'ms') {
+        diff = metric === 's' ?
+          diff / 1000 :
+          diff * 1000;
+      }
+
+      console.log(
+        `Function ${keyName} from class ${target.constructor.name} was executed in ${diff.toFixed(precision)} ${metric}`,
+      );
+
+      return result;
+    };
+  }
+}
